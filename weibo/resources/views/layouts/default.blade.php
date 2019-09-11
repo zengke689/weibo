8b7dccebb005e0de877d5ce0abbd6f0e74a76b23
zengke <497383788@qq.com>
2019-09-11 09:47:49 +0800

    初始化样式

--- weibo/resources/views/layouts/default.blade.php
+++ weibo/resources/views/layouts/default.blade.php
@@ -5,8 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-   @yield('content') 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+    </nav>
+    <div class="container">
+        @yield('content') 
+    </div>
 </body>
 </html>
